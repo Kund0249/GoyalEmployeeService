2cf5b697cfb9491922bb314beed64a16a351c81a
Kund0249 <k.kundan1999@gmail.com>
2023-09-30 14:03:16 +0530

    Add project files.

---
+++ GoyalEmployeeService/Controllers/DepartmentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataLayer.DataRepository;
+using DataLayer.DataEntity;
+
+namespace GoyalEmployeeService.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        private DepartmentRepository Db = new DepartmentRepository();
+
+        public IHttpActionResult GetDepartments()
+        {
+            List<DepartmentEntity> entities = Db.GetDepartments;
+            return Ok(entities);
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostDepartments(DepartmentEntity entity)
+        {
+            Db.Save(entity);
+            return Ok(entity);
+        }
+    }
+}
---
+++ GoyalEmployeeService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoyalEmployeeService.WebApiApplication" Language="C#" %>
